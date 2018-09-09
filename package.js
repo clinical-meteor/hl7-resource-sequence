6d51204209b67d44e9b56a2d342be6a8b20069a4
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 02:02:08 -0500

    QA pass

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-sequence',
-  version: '1.3.0',
+  version: '1.3.1',
   summary: 'HL7 FHIR Resource - Sequence',
   git: 'https://github.com/clinical-meteor/hl7-resource-sequence',
   documentation: 'README.md'
