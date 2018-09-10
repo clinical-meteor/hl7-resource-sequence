3fee98f1dbec488c1bddf500516cdb1cee31d234
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-09 20:59:58 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-sequence',
-  version: '1.3.1',
+  version: '1.3.2',
   summary: 'HL7 FHIR Resource - Sequence',
   git: 'https://github.com/clinical-meteor/hl7-resource-sequence',
   documentation: 'README.md'
@@ -35,5 +35,7 @@ Package.onUse(function (api) {
 });
 
 Npm.depends({
-  "simpl-schema": "1.5.3"
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
 })
